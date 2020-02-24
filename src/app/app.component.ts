7faaa15bc2f160aa49996301fcbb3d3103936039
Angel Munoz <angelmsanchez85@gmail.com>
2020-02-24 19:48:32 +0100

    lazy load component Foo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,11 @@
-import { Component, ViewChild, OnInit, AfterViewInit } from '@angular/core';
+import {
+  Component, ViewChild, OnInit,
+  AfterViewInit, Type
+} from '@angular/core';
 
 import { ViewChildComponent } from './view-child/view-child.component';
 import { ViewChildStaticComponent } from './view-child-static/view-child-static.component';
+import { FooComponent } from './foo/foo.component';
 
 @Component({
   selector: 'app-root',
@@ -9,10 +13,11 @@ import { ViewChildStaticComponent } from './view-child-static/view-child-static.
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit, AfterViewInit {
-  title = 'nine-and-a-half-weeks';
-
   @ViewChild(ViewChildComponent) private viewChild: ViewChildComponent;
   @ViewChild(ViewChildStaticComponent, { static: true }) private viewChildStatic: ViewChildStaticComponent;
+
+  title = 'nine-and-a-half-weeks';
+  foo: Promise<Type<FooComponent>>;
 
   ngOnInit(): void {
     this.viewChildStatic.testFunctionCallerByParentComponent();
@@ -22,8 +27,16 @@ export class AppComponent implements OnInit, AfterViewInit {
     this.viewChild.testFunctionCallerByParentComponent();
   }
 
+  loadFoo() {
+    if (!this.foo) {
+      this.foo = import(`./foo/foo.component`)
+        .then(({ FooComponent }) => FooComponent);
+    }
+  }
+
   testViewChild(): void {
     this.viewChild.testFunctionCallerByParentComponent();
     this.viewChildStatic.testFunctionCallerByParentComponent();
   }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ViewChildComponent } from './view-child/view-child.component';
 import { ViewChildStaticComponent } from './view-child-static/view-child-static.component';
+import { FooComponent } from './foo/foo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ViewChildComponent,
-    ViewChildStaticComponent
+    ViewChildStaticComponent,
+    FooComponent
   ],
   imports: [
     BrowserModule,
